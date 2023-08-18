d4ad1cfb56bb3a95eda735952ac4168f02735978
luisgg121 <luisgg12_1@hotmail.com>
2023-08-17 19:15:28 -0600

    Commit

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,7 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const API_KEY = '21f35088-7487-4357-a40c-88b7f5115160';
 export const MODAL_CLOSE_SEC = 2.5;
+export const KEY = 'f7005994-4290-46b3-9069-6f6751d8b53f';
+
+// exportar RESULT_PAGE_SIZE
+export const RESULT_PAGE_SIZE = 10;
